175aea7e87e356eb2649d0459b5224642a5a99ee
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2020-06-10 08:25:13 +0630

    Added Networking class

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by Myo Thura Zaw on 6/10/20.
+//
+
+import Foundation
+
+extension RazeCore {
+    public class Networking {
+        
+    }
+}
